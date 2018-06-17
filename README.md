23e316ffc76e9bc49427e68d5265b90da009c164
thesimpleblock <mpalts1234@hotmail.com>
2018-06-17 12:16:33 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Blinky
+A general-purpose Discord bot written in Java
+
+This bot is still in a very early stage development, expect bugs and errors
---
+++ src/test/java/test.java
@@ -0,0 +1,8 @@
+public class test {
+    public static void main(String[] args) {
+        //this is me testing array, please ignore thank you
+        String example = ".ping cleverbutt";
+        String[] array = example.split(" ");
+        System.out.println(array[0].startsWith("t."));
+    }
+}
